3f4dd953ba4099b0cd40790640e6c8a161e8c6e4
Aneri <aneri vala>
2018-01-01 00:46:29 +0530

    Changes in function and scss of Calendar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, Pipe, PipeTransform } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ElementRef, Renderer } from '@angular/core';
 import * as moment from 'moment';
 import * as _ from 'lodash';
 
@@ -7,59 +7,52 @@ export interface CalendarDate {
   selected?: boolean;
   today?: boolean;
 }
-@Pipe({ name: 'pairs' })
-export class PairsPipe implements PipeTransform {
-  transform(value: any) {
-    return value.filter((v, i) => i % 2 == 0).map((v, i) => [value[i * 2], value[i * 2 + 1]])
-  }
-}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   months;
   currentDate = moment();
-  nextDate;
-  defalutmonth;
   currentYear;
   uptoYear;
-
+  Currentmonth;
+  years = [];
   dayNames = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
   weeks: CalendarDate[][] = [];
   sortedDates: CalendarDate[] = [];
 
   @Input() selectedDates: CalendarDate[] = [];
   @Output() onSelectDate = new EventEmitter<CalendarDate>();
+ 
 
-  constructor() {
+  constructor(public el: ElementRef, public renderer: Renderer) {
     this.months = moment.monthsShort();
-    const currentMonth = moment().month();
-  
-     for (let resultIndex in this.months) {
-        console.log(resultIndex);
-   
-    }
   }
 
   ngOnInit(): void {
     this.currentYear = moment().year();
     this.uptoYear = this.currentYear + 1;
+    this.years.push(this.currentYear);
+    this.years.push(this.uptoYear);
+    this.Currentmonth = moment().format('MMM');
     this.generateCalendar();
+
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.selectedDates &&
       changes.selectedDates.currentValue &&
       changes.selectedDates.currentValue.length > 1) {
-      // sort on date changes for better performance when range checking
+
       this.sortedDates = _.sortBy(changes.selectedDates.currentValue, (m: CalendarDate) => m.mDate.valueOf());
       this.generateCalendar();
     }
   }
 
-  // date checkers
   isToday(date: moment.Moment): boolean {
     return moment().isSame(moment(date), 'day');
   }
@@ -72,22 +65,24 @@ export class AppComponent {
 
   isSelectedMonth(date: moment.Moment): boolean {
     return moment(date).isSame(this.currentDate, 'month');
-
   }
 
   selectDate(date: CalendarDate): void {
     this.onSelectDate.emit(date);
   }
+
   checkMonth(choosenMonth) {
     this.currentDate = moment().month(choosenMonth);
-
     this.generateCalendar();
+    this.Currentmonth = choosenMonth;
   }
+
   checkYear(choosenYear) {
     this.currentDate = moment().month(choosenYear);
-    this.generateCalendar();
-  }
+    //this.generateCalendar();
+    this.currentYear = choosenYear;
 
+  }
   // generate the calendar grid
   generateCalendar(): void {
     const dates = this.fillDates(this.currentDate);
@@ -96,7 +91,6 @@ export class AppComponent {
       weeks.push(dates.splice(0, 7));
     }
     this.weeks = weeks;
-
   }
 
   fillDates(currentMoment: moment.Moment): CalendarDate[] {
@@ -106,7 +100,6 @@ export class AppComponent {
     return _.range(start, start + 42)
       .map((date: number): CalendarDate => {
         const d = moment(firstDayOfGrid).date(date);
-
         return {
           today: this.isToday(d),
           selected: this.isSelected(d),
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PairsPipe } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PairsPipe
   ],
   imports: [
     BrowserModule,
